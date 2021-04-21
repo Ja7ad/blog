20dcd789ca514af0bac5cf6b0146fc13248ec35f
Javad <ja7adr@gmail.com>
2021-04-21 15:46:37 +0430

    update 2021/04/21

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210421.1525';
+const cacheName = 'chirpy-20210421.1546';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
