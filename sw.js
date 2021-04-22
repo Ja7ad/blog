b7d7639246d707ddf61db69860e4d4702b087bc9
Javad <ja7adr@gmail.com>
2021-04-22 11:54:05 +0430

    side bar color updated

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210422.1148';
+const cacheName = 'chirpy-20210422.1153';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
