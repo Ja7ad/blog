bf6c2a8d5f4ea4ee31a6119fbfe8c9d50d05202d
Javad <ja7adr@gmail.com>
2021-04-22 09:31:07 +0430

    updated

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -52,9 +52,6 @@ const resource = [
 /* The request url with below domain will be cached */
 const allowedDomains = [
   
-    'www.googletagmanager.com',
-    'www.google-analytics.com',
-  
 
   'blog.javad.tech',
 
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210421.2211';
+const cacheName = 'chirpy-20210422.0929';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
