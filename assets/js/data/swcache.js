f5c32b9b1e7555bdf01b21f8044f039e03ce0690
javad <ja7adr@gmail.com>
2021-05-27 13:36:40 +0430

    Update 27/05/2021

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -26,27 +26,19 @@ const resource = [
     '/about/',
   
 
-  /* --- Icons --- */
+  /* --- Favicons --- */
   
-  '/assets/img/favicons/favicon.ico',
-  '/assets/img/favicons/apple-icon.png',
-  '/assets/img/favicons/apple-icon-precomposed.png',
-  '/assets/img/favicons/apple-icon-57x57.png',
-  '/assets/img/favicons/apple-icon-60x60.png',
-  '/assets/img/favicons/apple-icon-72x72.png',
-  '/assets/img/favicons/apple-icon-76x76.png',
-  '/assets/img/favicons/apple-icon-114x114.png',
-  '/assets/img/favicons/apple-icon-120x120.png',
-  '/assets/img/favicons/apple-icon-144x144.png',
-  '/assets/img/favicons/apple-icon-152x152.png',
-  '/assets/img/favicons/apple-icon-180x180.png',
-  '/assets/img/favicons/android-icon-192x192.png',
-  '/assets/img/favicons/favicon-32x32.png',
-  '/assets/img/favicons/favicon-96x96.png',
+
+  '/assets/img/favicons/android-chrome-192x192.png',
+  '/assets/img/favicons/android-chrome-512x512.png',
+  '/assets/img/favicons/apple-touch-icon.png',
   '/assets/img/favicons/favicon-16x16.png',
-  '/assets/img/favicons/ms-icon-144x144.png',
-  '/assets/img/favicons/manifest.json',
+  '/assets/img/favicons/favicon-32x32.png',
+  '/assets/img/favicons/favicon.ico',
+  '/assets/img/favicons/mstile-150x150.png',
+  '/assets/img/favicons/site.webmanifest',
   '/assets/img/favicons/browserconfig.xml'
+
 ];
 
 /* The request url with below domain will be cached */
